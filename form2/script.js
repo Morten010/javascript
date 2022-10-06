03be46f675d7c6b7546ab5951ea07eacf0b475ce
Morten010 <97233551+Morten010@users.noreply.github.com>
2022-10-06 12:28:31 +0200

    updated

--- form2/script.js
+++ form2/script.js
@@ -2,14 +2,30 @@
 let userForm = document.forms["userform"];
 
 let inputName = document.getElementById("inputName");
-let userName = document.getElementById("username")
+let userName = document.getElementById("userName")
 
 function saveUserInput() {
 
     if(inputName.value != ""){
         
         console.log(inputName)
-        userName.innerHTML = inputName.value
+        userName.innerText = inputName.value
+
+    } else{
+        alert("du har glemt dit navn")
+    }
+    if(inputName.value != ""){
+        
+        console.log(inputName)
+        userName.innerText = inputName.value
+
+    } else{
+        alert("du har glemt dit navn")
+    }
+    if(inputName.value != ""){
+        
+        console.log(inputName)
+        userName.innerText = inputName.value
 
     } else{
         alert("du har glemt dit navn")
---
+++ toDoTask/javascript/app.js
@@ -0,0 +1,55 @@
+
+let userToDoList = ["Køb Ind til aftensmad", "Lav to do list", "Hent pakke i føtex"];
+let  toDoListUL = document.getElementById("toDoList");
+
+function addToDoList() {
+       
+    if(TodoTask["todoText"].value != ""){
+        userToDoList.push(TodoTask["todoText"].value);
+        console.log(userToDoList);
+
+        // const newListItem = document.createElement("li");
+        // newListItem.innerText = TodoTask["todoText"].value;
+
+        // toDoListUL.appendChild(newListItem);
+        document.getElementById("toDoList").innerHTML += 
+        `
+        <li>
+            <p>${TodoTask["todoText"].value}</p>
+            <i class="fa-regular fa-trash-can"></i>
+        </li>
+        ` 
+        
+     } else{
+         alert("Mangler text");
+     };
+};
+
+function ShowToDoList() {
+
+    for(let index = 0; index < userToDoList.length; index++){
+    
+        const newListItem = document.createElement("li");
+        newListItem.innerText = userToDoList[index];
+
+        toDoListUL.appendChild(newListItem);
+        
+    };
+    
+}
+function ShowToDoList() {
+
+    for(let index = 0; index < userToDoList.length; index++){
+    
+        document.getElementById("toDoList").innerHTML += 
+        `
+        <li>
+            <p>${userToDoList[index]}</p>
+            <i class="fa-regular fa-trash-can"></i>
+        </li>
+        ` 
+    };
+
+}
+
+ShowToDoList()
